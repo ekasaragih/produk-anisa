39d927f38792b1d54d9ada575985e456c4bb4e13
ekasaragih <saragiheka27@gmail.com>
2025-02-06 12:57:32 +0700

    PA-10: sertifikat initial page styling update

--- resources/views/feature/certificate.blade.php
+++ resources/views/feature/certificate.blade.php
@@ -1,149 +1,96 @@
 @extends('utils.layout.sidebar')
 
 @section('head')
-    <style>
-        .certificate-container {
-            border: 5px solid #4b7795;
-            padding: 30px;
-            background: linear-gradient(to right, rgba(32, 101, 239, 0.3), rgba(32, 198, 88, 0.2));
-            width: 90%;
-            margin: 50px auto;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-
-        .certificate-title {
-            font-size: 2.8em;
-            font-weight: bold;
-            color: #000000;
-            margin-bottom: 30px;
-            font-family: 'Times New Roman', Times, serif;
-        }
-
-        .certificate-subtitle {
-            font-size: 1.2em;
-            margin-bottom: 25px;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        .date-content {
-            font-size: 1.2em;
-            margin-top: 30px;
-            margin-bottom: 40px;
-            font-family: 'Times New Roman', Times, serif;
-        }
-
-        .certificate-content {
-            max-width: 900px;
-            margin: 0 auto;
-            padding: 0 20px;
-            text-align: center;
-        }
-
-        .certificate-content p {
-            font-size: 1.1em;
-            margin: 0;
-            line-height: 1.8;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-
-        .recipient-name {
-            font-size: 2em;
-            margin-bottom: 10px;
-            position: relative;
-            font-family: 'Times New Roman', Times, serif;
-        }
-
-        .recipient-name::after {
-            content: '';
-            position: absolute;
-            left: 50%;
-            bottom: 0;
-            width: 80%;
-            border-bottom: 2px solid #ffffff;
-            transform: translateX(-50%);
-        }
-
-        .signature {
-            margin-top: 40px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
-
-        .signature p {
-            font-size: 1.2em;
-            margin: 5px 0;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        .footer {
-            margin-top: 30px;
-            font-size: 0.8em;
-            color: #777;
-        }
-    </style>
 @endsection
 
 @section('content')
-    @include('utils.layout.topnav', ['title' => 'Sertifikat'])
-    <div class="container-fluid py-1 px-3">
-        <div class="row">
-            <div class="card" style="background-color: #F8FAFC;">
-                <div class="certificate-container">
-                    <div class="certificate-title">
-                        SERTIFIKAT PENCAPAIAN
-                    </div>
+@include('utils.layout.topnav', ['title' => 'Sertifikat'])
+<div class="container mx-auto pb-8 px-4 min-h-screen">
+    <div class="page p-8 bg-white rounded-2xl shadow-lg border border-gray-200 animate-fade-in">
+        <h2
+            class="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-l from-blue-600 to-teal-500 mb-6">
+            Sertifikat Saya
+        </h2>
+        <div class="grid place-items-center">
+            <div class="bg-white shadow-lg rounded-lg border-4 border-[#4b7795] w-full max-w-5xl p-4 
+            text-center items-center justify-center h-[280px] md:h-auto overflow-hidden"
+                style="background: linear-gradient(to right, rgba(32, 101, 239, 0.3), rgba(32, 198, 88, 0.2));">
 
-                    <div class="certificate-subtitle mt-4">
-                        Dengan bangga diberikan kepada:
-                    </div>
+                <h1 class="text-xs md:text-4xl font-bold text-black mb-3 md:mb-6">SERTIFIKAT PENCAPAIAN</h1>
 
-                    <div class="recipient-name">
-                        KUPA COCHANK
-                    </div>
+                <h2 class="text-[10px] md:text-lg font-semibold mb-2 md:mb-4">Dengan bangga diberikan kepada:</h2>
 
-                    <div class="certificate-content mt-4">
-                        <p>Sebagai penghargaan atas komitmen dan kedisiplinan menjalani pengobatan anemia selama
-                            <strong> 90 hari</strong>.
-                        </p>
-                        <p>Terima kasih telah menjadi inspirasi dalam menjaga kesehatan.</p>
-                        <br>
-                        <p>Teruslah melangkah menuju hidup yang lebih sehat!</p>
-                    </div>
+                <p class="text-sm md:text-3xl font-bold mb-1 md:mb-2 border-b border-white inline-block">KUPA COCHANK
+                </p>
 
-                    <p class="date-content">31 Desember 2025</p>
-
-                    <div class="signature">
-                        <div>
-                            <p>(Pemberi Sertifikat)</p>
-                        </div>
-                    </div>
-
-                    <div class="row d-flex justify-content-center align-items-center mt-4">
-                        <div class="col-auto">
-                            <img src="" class="mx-auto d-block" width="50" height="50" />
-                        </div>
-                        <div class="col-auto">
-                            <img src="" class="mx-auto d-block" width="50" height="50" />
-                        </div>
-                    </div>
-
-
+                <div class="mt-1 md:mt-4 text-gray-700 text-[8px] md:text-base">
+                    <p>Sebagai penghargaan atas komitmen dan kedisiplinan menjalani pengobatan anemia selama <strong>90
+                            hari</strong>.</p>
+                    <p>Terima kasih telah menjadi inspirasi dalam menjaga kesehatan.</p>
+                    <p class="mt-1 md:mt-2">Teruslah melangkah menuju hidup yang lebih sehat!</p>
                 </div>
+
+                <p class="text-[10px] md:text-lg mt-3 md:mt-6">31 Desember 2025</p>
+
+                <div class="mt-3 md:mt-6 flex flex-col items-center">
+                    <p class="text-[10px] md:text-lg">(Pemberi Sertifikat)</p>
+                </div>
+
+                <div class="flex justify-center mt-2 md:mt-6 space-x-2 md:space-x-4">
+                    <img src="" class="w-8 h-8 md:w-12 md:h-12" alt="Logo 1" />
+                    <img src="" class="w-8 h-8 md:w-12 md:h-12" alt="Logo 2" />
+                </div>
+
+
+            </div>
+            <!-- Buttons -->
+            <div class="mt-3 md:mt-6 flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0 sm:space-x-4">
+                <button onclick="downloadPDF()"
+                    class="text-xs md:text-base bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 md:px-5 md:py-2 rounded-md">
+                    Download PDF
+                </button>
+                <button onclick="shareCertificate()"
+                    class="text-xs md:text-base bg-teal-500 hover:bg-teal-600 text-white px-3 py-1 md:px-5 md:py-2 rounded-md">
+                    Share ke Media Sosial
+                </button>
             </div>
         </div>
-
-
-        {{-- DONT REPLACE THIS PART --}}
-        @include('utils.layout.footer')
     </div>
 
+    @include('utils.layout.footer')
+</div>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        // TO-DO: function download to pdf nya belum
+        function downloadPDF() {
+            const { jsPDF } = window.jspdf; 
+            const doc = new jsPDF();
+
+            doc.text("SERTIFIKAT PENCAPAIAN", 20, 20);
+            doc.text("Diberikan kepada: KUPA COCHANK", 20, 40);
+            doc.text("Sebagai penghargaan atas kedisiplinan menjalani pengobatan anemia selama 90 hari.", 20, 60);
+            doc.save("sertifikat.pdf");
+        }
+
+        function shareToSocial() {
+            if (navigator.share) {
+                navigator.share({
+                    title: "Sertifikat Pencapaian",
+                    text: "Saya telah berhasil menyelesaikan pengobatan anemia selama 90 hari! 🎉",
+                    url: window.location.href,
+                }).catch(error => console.log("Error sharing:", error));
+            } else {
+                alert("Browser tidak mendukung fitur berbagi.");
+            }
+        }
+
+        window.downloadPDF = downloadPDF;
+        window.shareCertificate = shareToSocial;
+    });
+</script>
 @endsection
