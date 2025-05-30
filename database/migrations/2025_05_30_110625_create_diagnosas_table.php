1b97268742433a32bf9272804f200f97883151c6
ekasaragih <saragiheka27@gmail.com>
2025-05-30 11:07:39 +0700

    PA-74: add migration for diagnosa table

---
+++ database/migrations/2025_05_30_110625_create_diagnosas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diagnosas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('diagnose_result');
+            $table->json('symptom_checks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diagnosas');
+    }
+};
